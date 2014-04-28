de9b65d7dee3aad9d8a45e0480ad3cef553b8e82
Kanchan Kumar <kankumar@cisco.com>
2014-04-28 13:11:36 +0530

    Added <- format of assignment.

--- run_analysis.R
+++ run_analysis.R
@@ -4,8 +4,8 @@
 setwd("C:\\Data Science\\3. Getting and Cleaning Data\\Week3\\UCI HAR Dataset")
 
 #Load subject data and combine into one data set
-subTest <- read.table(".\\test\\subject_test.txt")
-subTrain <- read.table(".\\train\\subject_train.txt")
+subTest <- read.table("./test/subject_test.txt")
+subTrain <- read.table("./train/subject_train.txt")
 allSub <- rbind(subTest, subTrain)
 colnames(allSub) <- "Subject"
 
@@ -25,21 +25,20 @@ allLabels[,1] <- as.vector(actLabels[match(allLabels$ActivityID, actLabels$Activ
 colnames(allLabels) <- c("ActivityName")
 
 #Load test and training inoutset and combine into one dataframe
-xTest <- read.table(".\\test\\X_test.txt")
-xTrain <- read.table(".\\train\\X_train.txt")
+xTest <- read.table("./test/X_test.txt")
+xTrain <- read.table("./train/X_train.txt")
 allInput <- rbind(xTest, xTrain)
 
 # Read the file features containing the feature names and assign it to the name of all Input
-featureTable = read.table(".\\features.txt")
-names(allInput) = featureTable[,2]
+featureTable <- read.table("./features.txt")
+names(allInput) <- featureTable[,2]
 
 #Subset the all Inout data with columns containing only mean and std in the column name
-meanStdColumns = c(grep("mean", featureTable[,2], ignore.case=TRUE), grep(c("std"), featureTable[,2], ignore.case=TRUE))
-#meanStdColumnNames = featureTable[meanStdColumns, 2]
-stdMeanInput = allInput[,meanStdColumns]
+meanStdColumns <- c(grep("mean", featureTable[,2], ignore.case=TRUE), grep(c("std"), featureTable[,2], ignore.case=TRUE))
+stdMeanInput <- allInput[,meanStdColumns]
 
 # append subject and label columns to the stdMeanInout dataset to get the tidy data as per requirement
-tidyData = cbind(allSub, allLabels, stdMeanInput)
+tidyData <- cbind(allSub, allLabels, stdMeanInput)
 
 # Group by Subject and ActivityName using melt and dcast function
 library(reshape2)
